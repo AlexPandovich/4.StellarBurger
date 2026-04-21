0f6a1271773ea5f3420bf97b65e7eee6ae16809e
Panda Note <alffgf@gmail.com>
2026-04-21 10:56:57 +0300

    show logged user in header menu

--- src/components/app-header/AppHeader.jsx
+++ src/components/app-header/AppHeader.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import styles from "./AppHeader.module.scss";
 
 import {
-  Button,
   Logo,
   BurgerIcon,
   ListIcon,
   ProfileIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import HeaderLink from "./header-link/header-link";
+import { useSelector } from "react-redux";
 
 const AppHeader = () => {
+  const user = useSelector((state) => state.user);
+  const userName = user.userName ? user.userName : "Personal account";
+
   return (
     <header className="p-4">
       <div className={`${styles.container} container`}>
@@ -21,11 +24,7 @@ const AppHeader = () => {
 
         <Logo className="outline" />
 
-        <HeaderLink
-          path="/profile"
-          Icon={ProfileIcon}
-          text="Personal account"
-        />
+        <HeaderLink path="/profile" Icon={ProfileIcon} text={userName} />
       </div>
     </header>
   );
